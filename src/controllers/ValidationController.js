6d539faf1f80720b8868aaf20b9eb71e45b23e46
Shiro-cha <noum.rzdr@gmail.com>
2022-12-02 19:26:50 +0300

    Push getjhistory

--- src/controllers/ValidationController.js
+++ src/controllers/ValidationController.js
@@ -2,27 +2,27 @@ const SimpleGit = require("simple-git");
 const fs = require("fs")
 const path = require("path");
 const pathToData = path.join(__dirname,"../../.data");
-const fileToIgnore = ["AdminDatabase"];
+const fileToAdd = ["backoffice-database"];
 class ValidationController {
   validate(req,res) {
     if(fs.existsSync(pathToData)){
       const git = SimpleGit(pathToData);
       if(!fs.existsSync(path.join(pathToData,".git"))){
         git.init(function(){
-          git.add(".",function(){
+          git.add(fileToAdd,function(){
             git.commit("new update",function(){
-              git.push("https://mitambatra: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@github.com/mitambatra/database.git","main",function(){
+              git.push(`https://${process.env.USERNAME}:${process.env.TOKEN}@${process.env.HOST}${process.env.REPOSISTORY}`,`${process.env.BRANCH}`,function(){
                 res.json({
                   message:"success"
                 })
               })
             })
           });
-        });
+        })
       }else{
         git.add(".",function(){
           git.commit("new update",function(){
-            git.push("https://mitambatra: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@github.com/mitambatra/database.git","master",function(err){
+            git.push(`https://${process.env.USERNAME}:${process.env.TOKEN}@${process.env.HOST}${process.env.REPOSISTORY}`,`${process.env.BRANCH}`,function(err){
               if(err){
                 console.log(err);
                 res.json(err)
@@ -40,5 +40,27 @@ class ValidationController {
     }
 
   }
+  getHistoryListe(req,res){
+
+    if(fs.existsSync(pathToData)){
+      const git = SimpleGit(pathToData);
+      if(fs.existsSync(path.join(pathToData,".git"))){
+        let data = []
+        git.log(function(err,logs){
+          console.log(logs);
+          logs["all"].forEach((log) => {
+            console.log(new Date(log.date));
+            data.push({hash:log.hash,date:log.date})
+          });
+
+          res.json(data);
+        })
+      }else {
+        throw new Error("Error in git database");
+      }
+    }
+
+  }
+  
 }
 module.exports = new ValidationController();
--- src/routes/api/index.router.js
+++ src/routes/api/index.router.js
@@ -2,6 +2,7 @@
 const router = require("express").Router()
 
 router.post("/validate",require("../../controllers/ValidationController").validate)
+router.post("/get-history",require("../../controllers/ValidationController").getHistoryListe)
 
 
 module.exports = router
