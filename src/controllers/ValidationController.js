d9ec629f10f8d7ec6c9c468bf69d350f3ff447ba
Shiro-cha <noum.rzdr@gmail.com>
2022-12-02 18:21:52 +0300

    Push validation

--- src/controllers/ValidationController.js
+++ src/controllers/ValidationController.js
@@ -1,8 +1,44 @@
-class LoginController {
+const SimpleGit = require("simple-git");
+const fs = require("fs")
+const path = require("path");
+const pathToData = path.join(__dirname,"../../.data");
+const fileToIgnore = ["AdminDatabase"];
+class ValidationController {
   validate(req,res) {
-    res.json({
-      message:"Validation Ok..."
-    })
+    if(fs.existsSync(pathToData)){
+      const git = SimpleGit(pathToData);
+      if(!fs.existsSync(path.join(pathToData,".git"))){
+        git.init(function(){
+          git.add(".",function(){
+            git.commit("new update",function(){
+              git.push("https://mitambatra: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@github.com/mitambatra/database.git","main",function(){
+                res.json({
+                  message:"success"
+                })
+              })
+            })
+          });
+        });
+      }else{
+        git.add(".",function(){
+          git.commit("new update",function(){
+            git.push("https://mitambatra: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@github.com/mitambatra/database.git","master",function(err){
+              if(err){
+                console.log(err);
+                res.json(err)
+              }else{
+                res.json({
+                  message:"success"
+                })
+              }
+
+            })
+          })
+        });
+
+      }
+    }
+
   }
 }
-module.exports = new LoginController();
+module.exports = new ValidationController();
